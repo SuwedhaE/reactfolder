e960a96145cf4efca4a8949c98874bb84714be11
Suwedha E <suwedhasai@gmail.com>
2023-11-17 15:38:20 +0530

    initial commit

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './Components/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
